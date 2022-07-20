00778d5da221916cd407812e05cb3a638f54d650
Fazal9999 <fazalurrehman442@gmail.com>
2022-07-20 13:12:18 +0500

    creating bottom nav

--- app/src/main/java/com/poonam/dua/activities/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/poonam/dua/activities/dashboard/DashboardActivity.kt
@@ -2,11 +2,17 @@ package com.poonam.dua.activities.dashboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.poonam.dua.R
+import com.poonam.dua.base.BindingActivity
+import com.poonam.dua.databinding.ActivityDashboardBinding
+import com.poonam.dua.databinding.ActivityMainBinding
 
-class DashboardActivity : AppCompatActivity() {
+class DashboardActivity: BindingActivity<ActivityDashboardBinding>()
+
+{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+
     }
 }
--- app/src/main/java/com/poonam/dua/utils/Utils.kt
+++ app/src/main/java/com/poonam/dua/utils/Utils.kt
@@ -1,11 +1,14 @@
 package com.poonam.dua.utils
 
 import android.content.Context
+import android.util.TypedValue
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
+
 
 object Utils {
     fun getDrawableInt(context: Context?, name: String?): Int {
@@ -27,4 +30,10 @@ object Utils {
             .transition(DrawableTransitionOptions.withCrossFade())
             .into(imageView)
     }
+    fun getAttrColor(context: Context, attr: Int): Int {
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(attr, typedValue, true)
+        return ContextCompat.getColor(context, typedValue.resourceId)
+    }
+
 }
